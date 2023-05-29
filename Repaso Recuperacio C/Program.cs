ed6cfcf6bc2e22e0cd7b9702a8c191e9e3b23040
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-05-29 12:05:39 +0200

    inicio

---
+++ Repaso Recuperacio C/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso_Recuperacio_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+        }
+    }
+}
