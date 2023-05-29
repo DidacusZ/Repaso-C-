43d94b5b095209fd78bb5fb5ae65fc54431ceaf1
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-05-29 13:36:36 +0200

    Regino lunes 29/05

---
+++ Repaso Recuperacio C/Persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso_Recuperacio_C
+{
+    internal class Persona
+    {
+        //atributos
+        string nombre;
+        string apellidos;
+        int anio, mes, dia;
+
+        //constructor vacio
+        public Persona()
+        {
+        }
+
+        //constructor
+        public Persona(string nombre, string apellidos, int dia, int mes, int anio )
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.anio = anio;
+            this.mes = mes;
+            this.dia = dia;
+        }
+
+        //getters y setters
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public int Anio { get => anio; set => anio = value; }
+        public int Mes { get => mes; set => mes = value; }
+        public int Dia { get => dia; set => dia = value; }
+
+        override// para sustituir el ToString de la clase object y que salga este 
+        public string ToString()
+        {
+            string concatenacion = "";
+
+            concatenacion = "Persona => [ Nombre = '" + nombre 
+                + "' Apellidos = '" + apellidos + "' Dia = '" + dia 
+                + "' Mes = '"+mes+"' Anio = '"+anio+"' ]";
+
+            return concatenacion;
+
+        }
+    }
+}
--- Repaso Recuperacio C/Program.cs
+++ Repaso Recuperacio C/Program.cs
@@ -10,14 +10,36 @@ namespace Repaso_Recuperacio_C
     {
         static void Main(string[] args)
         {
+            //constructor vacio
+            Persona per1 = new Persona();
 
+            //constructor todos los campos
+            Persona per2 = new Persona("regino","fdez",14,09,1991);
 
+            per1.Nombre = "diego";
+            per1.Apellidos = "de la prada";
 
+            Console.WriteLine("vacio");
+            Console.WriteLine("{0} {1}",per1.Nombre,per1.Apellidos);
+            Console.WriteLine("\ntodos campos");            
+            Console.WriteLine("{0} {1} {2}-{3}-{4}", per2.Nombre,per2.Apellidos,per2.Dia,per2.Mes,per2.Anio);
+            Console.WriteLine(per2.ToString());
+            Console.ReadKey();
 
+            //Pilares programacion orientada a objetos
 
+            //Polimorfismo -->
 
+            //Herencia -->
 
+            //Encapsulamiento --> dar privacidad a una clase,metodo,variable... (private)
+            //se utiliza para no tener una ....API (interfaz de programación de aplicaciones) pantalla inermedia, ofrece informacion
 
+            //Abstraccion --> ir paso por paso;
+            //sacar idea principal de cada parrafo;
+            //solo pensar en lo fundamental
+            //no definir nada hasta el momento de su uso (metodos)
+            //es como hacer un esquema de todo con las declaraciones de los metodos
         }
     }
 }
