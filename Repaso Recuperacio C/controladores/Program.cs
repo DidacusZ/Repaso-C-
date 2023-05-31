904f7bf528555acb641c91287cefff622a9316d0
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-05-31 13:34:42 +0200

    regino miercoles 31/05

--- Repaso Recuperacio C/controladores/Program.cs
+++ Repaso Recuperacio C/controladores/Program.cs
@@ -33,13 +33,20 @@ namespace Repaso_Recuperacio_C
             //Herencia -->
 
             //Encapsulamiento --> dar privacidad a una clase,metodo,variable... (private)
+            //controlar desde donde se puede acceder a nuestro codigo
             //se utiliza para no tener una ....API (interfaz de programación de aplicaciones) pantalla inermedia, ofrece informacion
 
-            //Abstraccion --> ir paso por paso;
+            //Abstraccion(trabajo) --> ir paso por paso;
             //sacar idea principal de cada parrafo;
             //solo pensar en lo fundamental
             //no definir nada hasta el momento de su uso (metodos)
             //es como hacer un esquema de todo con las declaraciones de los metodos
+
+            //crear todos metodos en interfaces,impl...  y que el codigo compile aunque no haga nada
+            //es como tener un esquema de todo el proyecto y luego definir los metodos
+
+            //Abstraccion(codigo) --> nombreInterfaz in = new nombreImpl();  esto es abstraccion
+
         }
     }
 }
---
+++ Repaso Recuperacio C/servicios/servicio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso_Recuperacio_C
+{
+    internal class servicio
+    {
+        //SERVICIOS: conjunto de metodos que hacen una funcionalidad determinada
+        //En cada clase los metodos se refiereen a lo mismo
+        //ej: alumnos--   clase:matricula-alumno...   alumno:añadir,eliminar-alumno...
+
+        public void Metodo1()
+        {
+
+        }
+
+        public void Metodo2()
+        {
+
+        }
+
+        public void Metodo3()
+        {
+
+        }
+
+    }
+}
